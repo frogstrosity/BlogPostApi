44c03159c0c660547e3a4774cdef10c1a5cfc683
frogstrosity <36r036@gmail.com>
2020-09-24 10:01:13 +0000

    add versioning package

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace BlogPostApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddApiVersioning(o => o.AssumeDefaultVersionWhenUnspecified = true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
